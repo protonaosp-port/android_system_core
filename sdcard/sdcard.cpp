ed3eae92cfe78c26d4265d06dc113897cbfb4e0f
Pierre-Hugues Husson <phh@phh.me>
2018-08-14 19:33:03 +0200

    Detect allowed sdcard options based on vndk
    Some kernel crashes when using too recent sdcardfs options
    
    Change-Id: I632e485f9b2a09a46d4a1fde2ea15217f8d92eff

--- sdcard/sdcard.cpp
+++ sdcard/sdcard.cpp
@@ -32,6 +32,7 @@
 #include <android-base/file.h>
 #include <android-base/logging.h>
 #include <android-base/macros.h>
+#include <android-base/properties.h>
 #include <android-base/stringprintf.h>
 #include <android-base/strings.h>
 
@@ -103,11 +104,12 @@ static bool sdcardfs_setup(const std::string& source_path, const std::string& de
                            mode_t mask, bool derive_gid, bool default_normal, bool unshared_obb,
                            bool use_esdfs) {
     // Add new options at the end of the vector.
+    int vndk = android::base::GetIntProperty<int>("ro.vndk.version", 29);
     std::vector<std::string> new_opts_list;
     if (multi_user) new_opts_list.push_back("multiuser,");
-    if (derive_gid) new_opts_list.push_back("derive_gid,");
-    if (default_normal) new_opts_list.push_back("default_normal,");
-    if (unshared_obb) new_opts_list.push_back("unshared_obb,");
+    if (derive_gid && vndk >= 27) new_opts_list.push_back("derive_gid,");
+    if (default_normal && vndk >= 28) new_opts_list.push_back("default_normal,");
+    if (unshared_obb && vndk >= 29) new_opts_list.push_back("unshared_obb,");
     // Try several attempts, each time with one less option, to gracefully
     // handle older kernels that aren't updated yet.
     for (int i = 0; i <= new_opts_list.size(); ++i) {
